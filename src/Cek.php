ddff7be053c6f4080b5d1dc03d91da96244b0be9
Putra Nugraha <69352047+PutraNugraha-new@users.noreply.github.com>
2021-01-24 11:40:45 +0700

    Memperbaiki Bug 1
    memperbaiki bug jika inputan string maka akan bernilai 0

--- src/Cek.php
+++ src/Cek.php
@@ -6,15 +6,20 @@ class Cek
 {
     public static function apakahGanjil($number)
     {
-        if ($number % 2 != 0) {
-            return true;
+        if(is_numeric($number)){
+            if ($number % 2 != 0) {
+                return true;
+            }
         }
+        
     }
 
     public static function apakahGenap($number)
     {
-        if ($number % 2 == 0) {
-            return true;
+        if(is_numeric($number)){
+            if ($number % 2 == 0) {
+                return true;
+            }
         }
     }
 }
